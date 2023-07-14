13fb01359b563ce462f9822039e386736b5919ef
MR.PARFENTYEV <110676672+MRPARFENTYEV@users.noreply.github.com>
2023-07-14 04:50:36 +0300

    reversing pages

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+# from views import home_view,time_view,workdir_view
 
 # Register your models here.
+# admin.site.register(home_view)
+# admin.site.register(time_view)
+# admin.site.register(workdir_view)
